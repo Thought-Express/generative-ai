147275e5c503374dc84e8b2badbf53a6e6e2f3c7
Khushal-Thanvi <khushal.thanvi@yahoo.com>
2026-01-27 19:13:45 +0530

    added the code from pii middleware

--- simple_agents/agent_with_middleware.py
+++ simple_agents/agent_with_middleware.py
@@ -4,13 +4,7 @@ import os
 from langchain.agents import create_agent
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-from langchain.tools import tool
-
-from langchain.agents.middleware import (
-    PIIMiddleware,
-    SummarizationMiddleware,
-    HumanInTheLoopMiddleware
-)
+from langchain.agents.middleware import PIIMiddleware
 
 load_dotenv(find_dotenv())
 
@@ -19,15 +13,19 @@ llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
 agent = create_agent(
     model=llm,
+    system_prompt="You are a helpful assistent.",
     middleware=[
-        PIIMiddleware("email", strategy="redact", apply_to_input=True)
-    ],
-    system_prompt="You are a helpful assistent."
+        PIIMiddleware("phone_number", detector=(
+            r"(?:\+?\d{1,3}[\s.-]?)?"
+            r"(?:\(?\d{2,4}\)?[\s.-]?)?"
+            r"\d{3,4}[\s.-]?\d{4}"
+        ), apply_to_input=True, strategy="redact")
+    ]
 )
 
 result = agent.invoke({
     "messages": [
-        {"role": "user", "content": "My email is khushal2182002@gmial.com. What is my email ?"}
+        {"role": "user", "content": "My email is +91 55555 55555. What is my email ?"}
     ]
 })
 
